6a3fa6786962e2790d48a3f3d7c224593a19a101
Adam Miskal <93220536+AdamMiskal@users.noreply.github.com>
2022-03-05 12:59:03 +0200

    Add files via upload

---
+++ Adam_Miskal_Assignment_3/Adam_Miskal_Assignment_3/Enums/Size.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adam_Miskal_Assignment_3.Enums
+{
+    public enum Size
+    {
+        XS,
+        S,
+        M,
+        L,
+        XL,
+        XXL,
+        XXXL
+    }
+}
---
+++ Adam_Miskal_Assignment_3/Adam_Miskal_Assignment_3/Models/TShirt.cs
@@ -0,0 +1,25 @@
+﻿using Adam_Miskal_Assignment_3.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adam_Miskal_Assignment_3.Models
+{
+    public class TShirt
+    {
+        public Color Color { get; set; }
+        public Fabric Fabric { get; set; }
+        public Size Size;
+        public decimal Price { get; set; }
+
+        public TShirt(Color color, Fabric fabric, Size size)
+        {
+            Color = color;
+            Fabric = fabric;
+            Size = size;
+        }
+
+    }
+}
